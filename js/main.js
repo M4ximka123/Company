d55d5d2f31e0a7f23c55965d58f35b9b59c1f240
M4ximka123 <max.show.200122@gmail.com>
2022-11-14 20:47:22 +0300

    complit project

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+   $('.services__slider').slick({
+      nextArrow: '<button type="button" class="slick-prev">Назад</button>',
+      prevArrow: '<button type="button" class="slick-next">След</button>',
+      responsive: [{
+         breakpoint: 1055,
+         settings: {
+          slidesToShow: 1  
+         },
+         breakpoint: 426,
+         settings: {
+            arrows: false,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 2300
+         }
+      }]
+   });
+
+   $('.burger-menu').on('click', function () {
+      $('.menu').toggleClass('menu--active');
+   });
+
+});
